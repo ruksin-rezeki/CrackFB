f7a63b37d2954b77b71e87a8987d8060191f839d
Black-IT <67135772+KINGTEBE-404@users.noreply.github.com>
2020-09-21 21:14:02 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+pip2 install bs4
+pip2 install tqdm
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/KINGTEBE-404/CrackFB
+cd CrackFB
+python2 crack.py
